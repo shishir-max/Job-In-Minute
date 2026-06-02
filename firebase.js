9629375ececb64200c62de826d3a42d4d5e17d1a
shishir-max <shishir@jobinminute.com>
2026-06-03 00:41:20 +0530

    Update Firebase SDK imports to version 10.8.0

--- firebase.js
+++ firebase.js
@@ -1,15 +1,9 @@
-// Firebase SDK Imports
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// firebase.js - Core SDK initialization
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-storage.js";
 
-import {
-  getAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-}
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
-// YOUR FIREBASE CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyAsgjVxoK6eJuWl-ofbL1VLEHXld13_wV0",
   authDomain: "job-in-minute.firebaseapp.com",
@@ -18,13 +12,12 @@ const firebaseConfig = {
   messagingSenderId: "507249342731",
   appId: "1:507249342731:web:0db15814ee9c454c8f0a0e"
 };
-// Initialize Firebase
+
+// Initialize Firebase App Instance
 const app = initializeApp(firebaseConfig);
 
+// Export instances to be imported by our app logic
+export { app };
 export const auth = getAuth(app);
-
-export {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut
-};
+export const db = getFirestore(app);
+export const storage = getStorage(app);
